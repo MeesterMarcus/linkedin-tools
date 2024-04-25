705d065e3f467e152ebb6a61bd44a52b8e1f284e
Marcus Lorenzana <mlorenzana@econtrols.com>
2024-04-25 14:04:13 -0500

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatCardModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'LinkedInTools';
+export class AppComponent implements OnInit {
+  expForm: FormGroup;
+  totalExpText: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.expForm = this.fb.group({
+      experiences: this.fb.array([])
+    });
+  }
+
+  ngOnInit(): void {
+    this.addExperienceInput(); // Initialize with one input group
+  }
+
+  get experiences(): FormArray {
+    return this.expForm.get('experiences') as FormArray;
+  }
+
+  addExperienceInput() {
+    const newExperience = new FormGroup({
+        yrsInput: new FormControl(0),
+        monthsInput: new FormControl(0, [Validators.max(11)]),
+    });
+    this.experiences.push(newExperience);
+}
+
+  calcTotalExperience() {
+    let totalYears = 0;
+    let totalMonths = 0;
+    this.experiences.controls.forEach((group) => {
+      totalYears += group.get('yrsInput')?.value;
+      totalMonths += group.get('monthsInput')?.value;
+    });
+
+    // Convert excess months to years
+    totalYears += Math.floor(totalMonths / 12);
+    totalMonths %= 12;
+    this.totalExpText = `${totalYears} years and ${totalMonths} months`
+    console.log(this.totalExpText);
+  }
 }
